5ffe71ad979d1fe0cb32d0a248a621ad961d0901
Hung En Hsieh <m1029003@astro.ncu.edu.tw>
2020-10-08 14:58:40 +0800

    encap function: read/pars/exec/free cmdline and add while-loop

--- shell/shell.c
+++ shell/shell.c
@@ -5,74 +5,101 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+int read_cmd (char buffer[]);
+int pars_cmd (char buffer[], char *arglist[]);
+int exec_cmd (char *arglist[]);
+int free_cmd (char *arglist[], int arg_num);
+
 int main (void)
 {
 	char buffer[512] = {0};
-	char c;
-	int pos = 0;
-	while (1) {
-		c = getchar();
-		if (c != '\n' && c != EOF) {
-			buffer[pos++] = c;
-		} else {
-			buffer[pos++] = '\0';
-			break;
-		}
-	}
-
 	char *arglist[20] = {0};
-	char *cp = buffer;
 	int arg_num = 0;
-	int len;
-	char *start;
-	while (*cp != '\0') {
-		while (*cp == ' ' || *cp == '\t')
-			cp++;
-		start = cp;
-		len = 1;
-		c = *++cp;
-		while (c != '\0' && c != ' ' && c != '\t') {
-			len++;
-			c = *++cp;
-		}
-		arglist[arg_num] = (char *) malloc(len + 1);
-		strncpy(arglist[arg_num], start, len);
-		arglist[arg_num][len] = '\0';
-		arg_num++;
+
+	while (1) {
+		read_cmd (buffer);
+		arg_num = pars_cmd (buffer, arglist);
+		exec_cmd (arglist);
+		free_cmd (arglist, arg_num);
+		memset (buffer, '\0', 512);
+		memset (arglist, 0, 80);
 	}
-#if 0
-	int i =0;
-	while (arglist[i]) {
-		printf("%s\n", arglist[i]);
-		free(arglist[i]);
-		i++;
-	}
-	return 0;
-#endif
-	int ret_from_fork;
-	int child_ret_status;
-	
-	ret_from_fork = fork();
-	
-	if (ret_from_fork == 0) {
-		execvp(arglist[0], arglist);
-		perror("execvp");
-		exit(EXIT_FAILURE);
-	} else if (ret_from_fork > 0) {
-		if (wait(&child_ret_status) ==  -1) {
-			perror("wait");
-			exit(EXIT_FAILURE);
-		}
-	} else {
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	
-	int i =0;
-    while (arglist[i])
- 	    free(arglist[i++]);
 
 	printf("main shell exit\n");
 
+	return 0;
+}
+
+int read_cmd (char buffer[])
+{
+    char c;
+    int pos = 0;
+    while (1) {
+        c = getchar();
+        if (c != '\n' && c != EOF) {
+            buffer[pos++] = c;
+        } else {
+            buffer[pos++] = '\0';
+            break;
+        }   
+    }
+    return pos;
+}
+
+int pars_cmd (char buffer[], char *arglist[])
+{
+	char *cp = buffer;
+    int arg_num = 0;
+    int len;
+    char *start;
+    char c;
+    while (*cp != '\0') {
+        while (*cp == ' ' || *cp == '\t')
+            cp++;
+        if (*cp == 0)
+			break;
+		start = cp;
+        len = 1;
+        c = *++cp;
+        while (c != '\0' && c != ' ' && c != '\t') {
+            len++;
+            c = *++cp;
+        }
+        arglist[arg_num] = (char *) malloc(len + 1);
+        strncpy(arglist[arg_num], start, len);
+        arglist[arg_num][len] = '\0';
+        arg_num++;
+    }
+	return arg_num;
+}
+
+int exec_cmd (char *arglist[])
+{
+	int ret_from_fork;
+    int child_ret_status;
+    
+    ret_from_fork = fork();
+    
+    if (ret_from_fork == 0) {
+        execvp(arglist[0], arglist);
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    } else if (ret_from_fork > 0) {
+        if (wait(&child_ret_status) ==  -1) {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+	return 0;
+}
+
+int free_cmd (char *arglist[], int arg_num)
+{
+	int i;
+    for (i = 0; i < arg_num; i++)
+        free(arglist[i]);	
 	return 0;
 }
