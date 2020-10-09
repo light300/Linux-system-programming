e60c5452402d74b37fea18bd0af6cfcbca423544
light300 <m1029003@gm.astro.ncu.edu.tw>
2020-10-09 16:19:53 +0800

    implement pipe

--- shell/shell.c
+++ shell/shell.c
@@ -10,7 +10,8 @@
 
 int read_cmd (char buffer[]);
 int pars_cmd (char buffer[], char *arglist[]);
-int exec_cmd (char *arglist[]);
+int exec_cmd (char *arglist[], int fd_in, int fd_out);
+int exec_cmdline  (char *arglist[], char buffer[]);
 int free_cmd (char *arglist[], int arg_num);
 void print_cmd_user(void);
 
@@ -20,18 +21,21 @@ int background_exec = 0;
 	0: normal mode
 */
 
+int pipe_mode = 0;
+/*
+    1: pipe mode
+    0: normal mode
+*/
+
 int main (void)
 {
 	char buffer[512] = {0};
 	char *arglist[20] = {0};
-	int arg_num = 0;
 
 	while (1) {
 		print_cmd_user();
 		read_cmd (buffer);
-		arg_num = pars_cmd (buffer, arglist);
-		exec_cmd (arglist);
-		free_cmd (arglist, arg_num);
+		exec_cmdline (arglist, buffer);
 		memset (buffer, '\0', 512);
 		memset (arglist, 0, 80);
 	}
@@ -109,22 +113,23 @@ int pars_cmd (char buffer[], char *arglist[])
 int is_builtin_cmd (char *arglist[]);
 int exec_builtin_cmd (char *arglist[]);
 
-int exec_cmd (char *arglist[])
+int exec_cmd (char *arglist[], int fd_in, int fd_out)
 {
-	if (arglist[0] == 0)
-		return 0;
-	if (is_builtin_cmd(arglist)) {
-		exec_builtin_cmd(arglist);
-		return 0;
-	}
-
 	int ret_from_fork;
-    int child_ret_status;
+    int child_ret_status = 0;
     
     ret_from_fork = fork();
     
     if (ret_from_fork == 0) {
 		signal(SIGINT, SIG_DFL);
+
+		dup2 (fd_in, 0);
+		dup2 (fd_out, 1);
+		if (fd_in != 0)
+            close (fd_in);
+        if (fd_out != 1)
+            close (fd_out);
+
         execvp(arglist[0], arglist);
         perror("execvp");
         exit(EXIT_FAILURE);
@@ -140,10 +145,65 @@ int exec_cmd (char *arglist[])
             	exit(EXIT_FAILURE);
         	}
 		}
+		
+		if (fd_in != 0)
+            close (fd_in);
+        if (fd_out != 1)
+            close (fd_out);
     } else {
         perror("fork");
         exit(EXIT_FAILURE);
     }
+	return child_ret_status;
+}
+
+int is_pipe_command (char buffer[])
+{
+    char *cp = buffer;
+    while (*cp != '\n' && *cp != '\0')
+    {
+        if (strncmp (cp, "|", 1) == 0)
+            return 1;
+        cp++;
+    }
+    return 0;
+}
+
+int exec_cmdline (char *arglist[], char buffer[])
+{
+	char *temp = buffer;
+	char *args;
+	int args_num = 0;
+	int fd_tmp = 0;
+
+	pipe_mode = is_pipe_command(buffer);
+
+	while ((args = strtok(temp, "|")) != NULL) {
+		temp = NULL;
+		args_num = pars_cmd(args, arglist);
+		if (arglist[0] == 0)
+			return 0;
+		if (is_builtin_cmd(arglist)) {
+        	exec_builtin_cmd(arglist);
+			return 0;
+		}
+		if (pipe_mode == 1) {
+			int fd[2];
+			if (pipe(fd) == -1) {
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
+			exec_cmd(arglist, fd_tmp, fd[1]);
+			fd_tmp = fd[0];
+			pipe_mode = 0;
+		} else {
+			exec_cmd(arglist, fd_tmp, 1);
+		}
+		free_cmd(arglist, args_num);
+        memset(arglist, 0, 80);
+	}
+	fd_tmp = 0;
+	pipe_mode = 0;
 	return 0;
 }
 
